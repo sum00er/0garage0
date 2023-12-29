f547c25f0a45a819f9f15a1056a43bff8b41f8ba
Summer <113702628+sum00er@users.noreply.github.com>
2023-12-29 21:16:24 +0800

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,53 +1,23 @@
-RegisterServerEvent('0garage0:updateOwnedVehicle')
-AddEventHandler('0garage0:updateOwnedVehicle', function(stored, Impound, vehicleProps)
-	local source = source
+ESX.RegisterServerCallback('0garage0:getVehicles', function(source, cb, parking)
 	local xPlayer  = ESX.GetPlayerFromId(source)
 
-		MySQL.update('UPDATE owned_vehicles SET `stored` = @stored, `vehicle` = @vehicle WHERE `plate` = @plate AND `owner` = @identifier',
-		{
-			['@identifier'] = xPlayer.identifier,
-			['@vehicle'] 	= json.encode(vehicleProps),
-			['@plate'] 		= vehicleProps.plate,
-			['@stored']     = stored,
-		})
+	MySQL.query('SELECT * FROM owned_vehicles WHERE owner = ?',{xPlayer.identifier}, 
+	function(result)
 
-		if stored then
-			xPlayer.showNotification(_U('veh_stored'))
-		end
-end)
-
-RegisterServerEvent('0garage0:setImpound')
-AddEventHandler('0garage0:setImpound', function(Impound, vehicleProps)
-	local source = source
-	local xPlayer  = ESX.GetPlayerFromId(source)
-
-		MySQL.update('UPDATE owned_vehicles SET `stored` = @stored, `vehicle` = @vehicle WHERE `plate` = @plate AND `owner` = @identifier',
-		{
-			['@identifier'] = xPlayer.identifier,
-			['@vehicle'] 	= json.encode(vehicleProps),
-			['@plate'] 		= vehicleProps.plate,
-			['@stored']     = false,
-		})
-
-		xPlayer.showNotification(_U('veh_impounded'))
-	
-end)
-
-
-ESX.RegisterServerCallback('0garage0:getVehiclesInParking', function(source, cb)
-	local xPlayer  = ESX.GetPlayerFromId(source)
-
-	MySQL.query('SELECT * FROM `owned_vehicles` WHERE `owner` = @identifier AND `stored` = 1',
-	{
-		['@identifier'] 	= xPlayer.identifier
-	}, function(result)
-
-		local vehicles = {}
+		local vehicles = {['0'] = {}, ['1'] = {}}
 		for i = 1, #result, 1 do
-			table.insert(vehicles, {
-				vehicle 	= json.decode(result[i].vehicle),
-				plate 		= result[i].plate
-			})
+			if (not result[i].image and not result[i].label) or type(result[i].stored) ~= 'number' then
+				print('[ERROR] Please run SQL before using the script')
+				return
+			end
+			if result[i].stored == 0 or not parking or result[i].parking == parking then
+				table.insert(vehicles[tostring(result[i].stored)], {
+					vehicle 	= json.decode(result[i].vehicle),
+					plate 		= result[i].plate,
+					image		= result[i].image,
+					label		= result[i].label 
+				})
+			end
 		end
 
 		cb(vehicles)
@@ -57,11 +27,8 @@ end)
 ESX.RegisterServerCallback('0garage0:checkVehicleOwner', function(source, cb, plate)
     local xPlayer = ESX.GetPlayerFromId(source)
 
-	MySQL.query('SELECT COUNT(*) as count FROM `owned_vehicles` WHERE `owner` = @identifier AND `plate` = @plate',
-	{
-		['@identifier'] 	= xPlayer.identifier,
-		['@plate']     		= plate
-	}, function(result)
+	MySQL.query('SELECT COUNT(*) as count FROM owned_vehicles WHERE owner = ? AND `plate` = ?',{xPlayer.identifier, plate},
+	function(result)
 
 		if tonumber(result[1].count) > 0 then
 			return cb(true)
@@ -71,42 +38,71 @@ ESX.RegisterServerCallback('0garage0:checkVehicleOwner', function(source, cb, pl
 	end)
 end)
 
-ESX.RegisterServerCallback('0garage0:getVehiclesInPound', function(source, cb)
+RegisterServerEvent('0garage0:updateOwnedVehicle')
+AddEventHandler('0garage0:updateOwnedVehicle', function(stored, parking, vehicleProps, entity, shouldPay)
+	local source = source
 	local xPlayer  = ESX.GetPlayerFromId(source)
-
-	MySQL.query('SELECT * FROM `owned_vehicles` WHERE `owner` = @identifier AND `stored` = @stored',
-	{
-		['@identifier'] 	= xPlayer.identifier,
-		['@stored'] = 0 or 2
-	}, function(result)
-		local vehicles = {}
-
-		for i = 1, #result, 1 do
-			table.insert(vehicles, {
-				vehicle 	= json.decode(result[i].vehicle),
-				plate 		= result[i].plate
-			})
+	if entity and stored == 1 then
+		local Vehicle = NetworkGetEntityFromNetworkId(entity)
+		if DoesEntityExist(Vehicle) then
+			DeleteEntity(Vehicle)
 		end
+	end
+	if shouldPay then
+		if xPlayer.getMoney() >= Config.ImpoundCost then
+			xPlayer.removeMoney(Config.ImpoundCost, "Impound Fee")
+			TriggerClientEvent('esx:showNotification', source, _U('pay_Impound_bill', Config.ImpoundCost))
+		else
+			TriggerClientEvent('esx:showNotification', source,_U('missing_money'))
+			local Vehicle = NetworkGetEntityFromNetworkId(entity)
+			if DoesEntityExist(Vehicle) then
+				DeleteEntity(Vehicle)
+			end
+			return
+		end
+	end
 
-		cb(vehicles)
-	end)
+	MySQL.update('UPDATE owned_vehicles SET stored = ?, vehicle = ?, parking = ? WHERE plate = ? AND owner = ?',{stored, json.encode(vehicleProps), parking, vehicleProps.plate, xPlayer.identifier})
+	print(stored)
+	if stored == 1 then
+		TriggerClientEvent('esx:showNotification', source, _U('veh_stored'))
+	end
+		
 end)
 
-ESX.RegisterServerCallback('0garage0:checkMoney', function(source, cb, amount)
-	local xPlayer = ESX.GetPlayerFromId(source)
-
-	cb(xPlayer.getMoney() >= amount)
-end)
-
-RegisterServerEvent("0garage0:payPound")
-AddEventHandler("0garage0:payPound", function(amount)
+ESX.RegisterServerCallback('0garage0:delVehBeforeRetrieve', function(source, cb, plate)
+	if Config.retrieveVerify then
 		local source = source
-    local xPlayer = ESX.GetPlayerFromId(source)
-
-    if xPlayer.getMoney() >= amount then
-        xPlayer.removeMoney(amount, "Impound Fee")
-				xPlayer.showNotification(_U('pay_Impound_bill', amount))
-    else
-		xPlayer.showNotification(_U('missing_money'))
-    end
+		local veh = GetAllVehicles()
+		local my_ped = GetPlayerPed(source)
+		local found, canDel = false, nil
+		for k, v in pairs(veh) do
+			local veh_plate = ESX.Math.Trim(GetVehicleNumberPlateText(v))
+			if veh_plate == plate then
+				found = true
+					local last_ped = GetLastPedInVehicleSeat(v, -1)
+					if last_ped == my_ped or last_ped == 0 then
+						canDel = v
+					elseif GetVehicleEngineHealth(v) <= 0 or GetVehicleBodyHealth(v) <= 0 then
+						canDel = v
+					end
+				break
+			end
+		end
+		local xPlayer = ESX.GetPlayerFromId(source)
+		if found and canDel then
+			if DoesEntityExist(canDel) then
+				DeleteEntity(canDel)
+			end
+			TriggerClientEvent('esx:showNotification', source, '已刪除仍存在的車輛')
+			cb(true) 
+		elseif not found then
+			cb(true)
+		else
+			TriggerClientEvent('esx:showNotification', source, '車輛依然存在，無法領取')
+			cb(false)
+		end
+	else
+		cb(true)
+	end
 end)
