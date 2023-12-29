b3ed76e3b42c4bf56a852dd660dd7f6f00581b78
Summer <113702628+sum00er@users.noreply.github.com>
2023-12-29 21:15:40 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,5 @@
 Config = {}
-Config.Locale = "en"
-
-Config.oldESX = false
+Config.Locale = "zh"
 
 Config.DrawDistance = 10.0
 
@@ -9,149 +7,144 @@ Config.DrawDistanceStop = 20.0
 
 Config.ImpoundCost = 3000
 
+Config.SeperateGarage = false
+
 Config.retrieveVerify = true
+
+Config.TextUI = function(text)
+    ESX.ShowHelpNotification(text)
+    -- local isOpen, current_text = lib.isTextUIOpen()
+    -- if not isOpen or current_text ~= text then
+    --     lib.showTextUI(text, {
+    --         icon = 'car',
+    --         style = {
+    --             borderRadius = 0.2,
+    --             backgroundColor = '#BB2649',
+    --             color = 'white'
+    --         }
+    --     })
+    -- end
+end
+
+Config.CloseUI = function()
+    -- lib.hideTextUI()
+end
 
 Config.Markers = {
 	EntryPoint = {
-		Type = 21,
+		Type = 27,
 		Size = {
 			x = 1.0,
 			y = 1.0,
 			z = 0.5,
 		},
 		Color = {
-			r = 50,
-			g = 200,
-			b = 50,
+			r = 187,
+			g = 38,
+			b = 73,
 		},
 	},
 	StopPoint = {
-		Type = 21,
+		Type = 27,
 		Size = {
-			x = 1.0,
-			y = 1.0,
+			x = 2.0,
+			y = 2.0,
 			z = 0.5,
 		},
 		Color = {
-			r = 50,
-			g = 200,
-			b = 50,
+			r = 187,
+			g = 38,
+			b = 73,
 		},
 	},
-	GetOutPoint = {
-		Type = 21,
+	GetOutPoint = { 
+		Type = 27,
 		Size = {
 			x = 1.0,
 			y = 1.0,
 			z = 0.5,
 		},
 		Color = {
-			r = 200,
-			g = 51,
-			b = 51,
+			r = 187,
+			g = 38,
+			b = 73,
 		},
 	},
 }
 
 Config.Garages = {
+    --garages
 	VespucciBoulevard = {
-		EntryPoint = {
-			x = -285.2,
-			y = -886.5,
-			z = 31.0,
-		},
-		SpawnPoint = {
-			x = -309.3,
-			y = -897.0,
-			z = 31.0,
-			heading = 351.8,
-		},
-		StopPoint = {
-			x = -302.35,
-			y = -899.31,
-			z = 31.0,
-		},
-		Sprite = 357,
-		Scale = 0.8,
-		Colour = 3,
+        coords = {
+            EntryPoint = vec(-285.2, -886.5, 31.0),
+            StopPoint = vec(-302.35, -899.31, 31.0),
+        },
+        SpawnPoint = vec(-309.3, -897.0, 31.0, 351.8),
+		blip = {
+            Coords = vec(-285.2, -886.5, 31.0),
+            Enable = true,
+            Sprite = 357,
+		    Scale = 0.8,
+		    Colour = 3,
+            Text = 'Garage'
+        }
 	},
 	SanAndreasAvenue = {
-		EntryPoint = {
-			x = 216.57,
-			y = -809.83,
-			z = 30.8,
-		},
-		SpawnPoint = {
-			x = 230.09,
-			y = -798.7,
-			z = 30.8,
-			heading = 158.8,
-		},
-		StopPoint = {
-			x = 216.4,
-			y = -786.6,
-			z = 30.8,
-		},
-		Sprite = 357,
-		Scale = 0.8,
-		Colour = 3,
+        coords = {
+            EntryPoint = vec(216.57, -809.83, 30.8),
+            StopPoint = vec(216.4, -786.6, 30.8),
+        },
+        SpawnPoint = vec(230.09, -798.7, 30.8, 158.8),
+		blip = {
+            Coords = vec(216.57, -809.83, 30.8),
+            Enable = true,
+            Sprite = 357,
+		    Scale = 0.8,
+		    Colour = 3,
+            Text = 'Garage'
+        }
 	},
-}
-
-Config.Impounds = {
-	LosSantos = {
-		GetOutPoint = {
-			x = 400.7,
-			y = -1630.5,
-			z = 29.3,
-		},
-		SpawnPoint = {
-			x = 401.9,
-			y = -1647.4,
-			z = 29.2,
-			heading = 323.3,
-		},
-		Sprite = 524,
-		Scale = 0.8,
-		Colour = 1,
+    --impounds
+    LosSantos = {
+        coords = {
+            GetOutPoint = vec(400.7, -1630.5, 29.3)
+        },
+		SpawnPoint = vec(404.2541, -1643.5513, 29.2919, 222.4407),
+        blip = {
+            Coords = vec(400.7, -1630.5, 29.3),
+            Enable = true,
+            Sprite = 524,
+            Scale = 0.8,
+            Colour = 1,
+            Text = 'Impound'
+        }
 	},
 	PaletoBay = {
-		GetOutPoint = {
-			x = -211.4,
-			y = 6206.5,
-			z = 31.4,
-		},
-		SpawnPoint = {
-			x = -204.6,
-			y = 6221.6,
-			z = 30.5,
-			heading = 227.2,
-		},
-		Sprite = 524,
-		Scale = 0.8,
-		Colour = 1,
+        coords = {
+            GetOutPoint = vec(-211.4, 6206.5, 31.4)
+        },
+		SpawnPoint = vec(-204.6, 6221.6, 30.5, 227.2),
+		blip = {
+            Coords = vec(-211.4, 6206.5, 31.4),
+            Enable = true,
+            Sprite = 524,
+            Scale = 0.8,
+            Colour = 1,
+            Text = 'Impound'
+        }
 	},
 	SandyShores = {
-		GetOutPoint = {
-			x = 1728.2,
-			y = 3709.3,
-			z = 33.2,
-		},
-		SpawnPoint = {
-			x = 1722.7,
-			y = 3713.6,
-			z = 33.2,
-			heading = 19.9,
-		},
-		Sprite = 524,
-		Scale = 0.8,
-		Colour = 1,
+        coords = {
+            GetOutPoint = vec(1645.23, 3807.01, 35.12)
+        },
+		SpawnPoint = vec(1643.63, 3801.42, 35.01, 142.66),
+		blip = {
+            Coords = vec(1645.23, 3807.01, 35.12),
+            Enable = true,
+            Sprite = 524,
+            Scale = 0.8,
+            Colour = 1,
+            Text = 'Impound'
+        }
 	},
 }
-
-exports("getGarages", function()
-	return Config.Garages
-end)
-exports("getImpounds", function()
-	return Config.Impounds
-end)
